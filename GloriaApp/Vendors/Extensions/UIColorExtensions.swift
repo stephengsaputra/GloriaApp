928b73215a5178904de2265fd49d34516af9dd18
Stephen Giovanni Saputra <stephen.gsaputra@gmail.com>
2022-11-12 17:17:57 +0700

    Added UIColor Extensions

--- GloriaApp/Vendors/Extensions/UIColorExtensions.swift
+++ GloriaApp/Vendors/Extensions/UIColorExtensions.swift
@@ -5,4 +5,20 @@
 //  Created by Stephen Giovanni Saputra on 12/11/22.
 //
 
-import Foundation
+import UIKit
+
+extension UIColor {
+    
+    static let primaryColor = UIColor.init(named: "primaryColor")
+    static let secondaryColor = UIColor.init(named: "secondaryColor")
+    
+    static let backgroundColor = UIColor.init(named: "backgroundColor")
+    
+    static let contentBG = UIColor.init(named: "contentBG")
+    static let contentBorder = UIColor.init(named: "contentColor")
+    
+    static let dropShadow = UIColor.init(named: "dropShadow")
+    
+    static let tfBG = UIColor.init(named: "tfBG")
+    static let tfBorder = UIColor.init(named: "tfBorder")
+}
