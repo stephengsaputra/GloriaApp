e3b26a26c5bcb3a410dc7a1419e781421804d03d
Stephen Giovanni Saputra <stephen.gsaputra@gmail.com>
2022-11-12 16:42:52 +0700

    Initial Commit

---
+++ GloriaApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GloriaApp
+//
+//  Created by Stephen Giovanni Saputra on 12/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
